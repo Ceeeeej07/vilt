7f13df41f7ba5e5fe9074603a9bb21443349d85e
Carl J <cjcsantoceldes07@gmail.com>
2025-10-22 19:37:52 +0800

    Learning Routes

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="text-3xl font-bold underline">Home</h1>
+        <h1 class="text-3xl font-bold underline">ABOUT</h1>
         <p class="mt-4">TESTING VUE COMMIT VIDEO 1</p>
     </div>
 </template>
