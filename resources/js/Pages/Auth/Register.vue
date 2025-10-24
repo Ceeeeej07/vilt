830677dfad9e9e3638eb452be57f56fd7588cb72
Carl J <cjcsantoceldes07@gmail.com>
2025-10-24 21:57:17 +0800

    Learning: components

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useForm } from "@inertiajs/vue3";
+import TextInput from "../Components/TextInput.vue";
 
 const registerForm = useForm({
     name: "",
@@ -24,66 +25,30 @@ const submit = () => {
 
         <div class="w-full max-w-md mt-6">
             <form @submit.prevent="submit">
-                <div class="flex flex-col gap-4 w-full mt-4">
-                    <label class="font-bold">Name</label>
-                    <input
-                        v-model="registerForm.name"
-                        class="rounded-2xl border border-gray-300 bg-gray-50 px-3 h-12 text-base text-black placeholder:text-black placeholder:opacity-50 outline-none transition duration-200 focus:border-black focus:ring-2 focus:ring-black"
-                        type="text"
-                        required
-                        placeholder="Name"
-                    />
-                    <small
-                        class="text-red-600"
-                        v-if="registerForm.errors.name"
-                        >{{ registerForm.errors.name }}</small
-                    >
-                </div>
-                <div class="flex flex-col gap-4 w-full mt-4">
-                    <label class="font-bold">Email</label>
-                    <input
-                        v-model="registerForm.email"
-                        class="rounded-2xl border border-gray-300 bg-gray-50 px-3 h-12 text-base text-black placeholder:text-black placeholder:opacity-50 outline-none transition duration-200 focus:border-black focus:ring-2 focus:ring-black"
-                        type="text"
-                        required
-                        placeholder="Email@example.com"
-                    />
-                    <small
-                        class="text-red-600"
-                        v-if="registerForm.errors.email"
-                        >{{ registerForm.errors.email }}</small
-                    >
-                </div>
-                <div class="flex flex-col gap-4 w-full mt-4">
-                    <label class="font-bold">Password</label>
-                    <input
-                        v-model="registerForm.password"
-                        class="rounded-2xl border border-gray-300 bg-gray-50 px-3 h-12 text-base text-black placeholder:text-black placeholder:opacity-50 outline-none transition duration-200 focus:border-black focus:ring-2 focus:ring-black"
-                        type="password"
-                        required
-                        placeholder="Password"
-                    />
-                    <small
-                        class="text-red-600"
-                        v-if="registerForm.errors.password"
-                        >{{ registerForm.errors.password }}</small
-                    >
-                </div>
-                <div class="flex flex-col gap-4 w-full mt-4">
-                    <label class="font-bold">Confirm Password</label>
-                    <input
-                        v-model="registerForm.password_confirmation"
-                        class="rounded-2xl border border-gray-300 bg-gray-50 px-3 h-12 text-base text-black placeholder:text-black placeholder:opacity-50 outline-none transition duration-200 focus:border-black focus:ring-2 focus:ring-black"
-                        type="password"
-                        required
-                        placeholder="Confirm Password"
-                    />
-                    <small
-                        class="text-red-600"
-                        v-if="registerForm.errors.password_confirmation"
-                        >{{ registerForm.errors.password_confirmation }}</small
-                    >
-                </div>
+                <TextInput
+                    v-model="registerForm.name"
+                    name="Name"
+                    type="text"
+                    :message="registerForm.errors.name"
+                />
+                <TextInput
+                    v-model="registerForm.email"
+                    name="Email"
+                    type="email"
+                    :message="registerForm.errors.email"
+                />
+                <TextInput
+                    v-model="registerForm.password"
+                    name="Password"
+                    type="password"
+                    :message="registerForm.errors.password"
+                />
+                <TextInput
+                    v-model="registerForm.password_confirmation"
+                    name="Confirm Password"
+                    type="password"
+                    :message="registerForm.errors.password_confirmation"
+                />
                 <button
                     @click.prevent="submit"
                     class="disabled:opacity-50 mt-6 w-full bg-green-800 text-white font-bold py-3 px-6 rounded-2xl hover:bg-green-900 transition duration-200"
---
+++ resources/js/Pages/Components/TextInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+defineProps({
+    name: { type: String, required: true },
+    type: { type: String, default: "text" },
+    message: String,
+});
+</script>
+
+<template>
+    <div class="flex flex-col gap-4 w-full mt-4">
+        <label class="font-bold">{{ name }}</label>
+        <input
+            v-model="model"
+            class="rounded-2xl border border-gray-300 bg-gray-50 px-3 h-12 text-base text-black placeholder:text-black placeholder:opacity-50 outline-none transition duration-200 focus:border-black focus:ring-2 focus:ring-black"
+            :type="text"
+            required
+            placeholder="Name"
+        />
+        <small class="text-red-600" v-if="message">{{ message }}</small>
+    </div>
+</template>
